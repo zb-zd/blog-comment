fc912afd575d20b2d621f5bcfe22333c640d5c2a
日渐肥胖的左 <2712104231@qq.com>
2023-04-01 11:15:23 +0800

    初始化blog

---
+++ .vuepress/config.ts
@@ -0,0 +1,108 @@
+import { defineUserConfig } from "vuepress";
+import type { DefaultThemeOptions } from "vuepress";
+import recoTheme from "vuepress-theme-reco";
+
+export default defineUserConfig({
+  title: "日渐肥胖的左",
+  description: "只是在玩",
+  theme: recoTheme({
+    style: "@vuepress-reco/style-default",
+    logo: "/logo.png",
+    author: "日渐肥胖的左",
+    authorAvatar: "/head.JPG",
+    docsRepo: "",
+    docsBranch: "main",
+    docsDir: "example",
+    lastUpdatedText: "",
+    // series 为原 sidebar
+    series: {
+      "/docs/theme-reco/": [
+        {
+          text: "module one",
+          children: ["home", "theme"],
+        },
+        {
+          text: "module two",
+          children: ["api", "plugin"],
+        },
+      ],
+    },
+    navbar: [
+      { text: "首页", link: "/" },
+      { text: "分类", link: "/categories/reco/1/" },
+      { text: "标签", link: "/tags/tag1/1/" },
+      {
+        text: "文档",
+        children: [
+          { text: "主题模块", link: "/docs/theme-reco/theme" },
+          { text: "引导模块", link: "/blogs/other/guide" },
+        ],
+      },
+    ],
+    bulletin: {
+      body: [
+        {
+          type: "text",
+          content: `🎉🎉🎉 日渐肥胖的左个人网站2.x版本。`,
+          style: "font-size: 12px;",
+        },
+        // {
+        //   type: "hr",
+        // },
+        // {
+        //   type: "title",
+        //   content: "QQ 群",
+        // },
+        // {
+        //   type: "text",
+        //   content: `
+        //   <ul>
+        //     <li>QQ群1：1037296104</li>
+        //     <li>QQ群2：1061561395</li>
+        //     <li>QQ群3：962687802</li>
+        //   </ul>`,
+        //   style: "font-size: 12px;",
+        // },
+        // {
+        //   type: "hr",
+        // },
+        // {
+        //   type: "title",
+        //   content: "GitHub",
+        // },
+        // {
+        //   type: "text",
+        //   content: `
+        //   <ul>
+        //     <li><a href="https://github.com/vuepress-reco/vuepress-theme-reco-next/issues">Issues<a/></li>
+        //     <li><a href="https://github.com/vuepress-reco/vuepress-theme-reco-next/discussions/1">Discussions<a/></li>
+        //   </ul>`,
+        //   style: "font-size: 12px;",
+        // },
+        // {
+        //   type: "hr",
+        // },
+        // {
+        //   type: "buttongroup",
+        //   children: [
+        //     {
+        //       text: "打赏",
+        //       link: "/docs/others/donate.html",
+        //     },
+        //   ],
+        // },
+      ],
+    },
+    // valineConfig 配置与 1.x 一致
+    // valineConfig: {
+    //   appId: 'xxx',
+    //   appKey: 'xxx',
+    //   placeholder: '填写邮箱可以收到回复提醒哦！',
+    //   verify: true, // 验证码服务
+    //   // notify: true,
+    //   recordIP: true,
+    //   // hideComments: true // 隐藏评论
+    // },
+  }),
+  // debug: true,
+});
---
+++ README.md
@@ -0,0 +1,32 @@
+---
+home: true
+modules:
+  - BannerBrand
+  - Blog
+  - MdContent
+  - Footer
+bannerBrand:
+  bgImage: '/bg.svg'
+  title: 左的个人站
+  description: 记录日常中的点点滴滴
+  tagline: 记录日常中的点点滴滴
+  # bgImageStyle: 
+    # height: 700px
+  buttons:
+    - { text: Guide, link: '/docs/guide/introduce' }
+    - { text: Default Style, link: '/docs/style-default-api/introduce', type: 'plain' }
+  socialLinks:
+    - { icon: 'LogoGithub', link: 'https://github.com/vuepress-reco/vuepress-theme-reco' }
+blog: # 博客模块配置
+  # socialLinks: # 社交地址
+  #   - { icon: 'LogoGithub', link: 'https://github.com/recoluan' }
+# footer: 
+#   record: 域名备案文案
+#   recordLink: 域名备案地址
+#   cyberSecurityRecord: 公安备案文案
+#   cyberSecurityLink: 公安备案地址
+#   startYear: 2018
+isShowTitleInHome: true
+actionText: About
+actionLink: /views/other/about
+---
